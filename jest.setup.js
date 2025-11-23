4c09b66a2a32066151c57d39a0033912bc51e965
arielvainstein <arielvans@gmail.com>
2025-11-23 11:16:59 -0300

    chore: add jest dependencies and config setup

---
+++ jest.setup.js
@@ -0,0 +1,26 @@
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom'
+
+// Mock Next.js router
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+      back: jest.fn(),
+      pathname: '/',
+      query: {},
+      asPath: '/',
+    }
+  },
+  usePathname() {
+    return '/'
+  },
+}))
+
+// Mock server actions
+jest.mock('@/actions/chat-actions', () => ({
+  askAgent: jest.fn(),
+  validateAIConfig: jest.fn(),
+}))
